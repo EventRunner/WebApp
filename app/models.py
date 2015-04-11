31e3b2af016f085326123654f905c428fad3181d
Qiuwen "Owen" Fan <fanqiuwen@gmail.com>
2015-04-10 20:54:03 -0400

    Fix mixed indentation

--- app/models.py
+++ app/models.py
@@ -10,32 +10,32 @@ from app import db, login_manager
 ##################################################
 
 
-						   
+
 ##################################################
 # Models
 # These should inherit from db.Model if they have an associated table
 ##################################################
 
 class User(db.Model, UserMixin):
-	"""Represents a user."""
-	id = db.Column(db.Integer, primary_key=True)
-	name = db.Column(db.String(64))
-	email = db.Column(db.String(128), unique=True)
-	password = db.Column(db.LargeBinary(4096))
-	
-	# This is a list of characters designating what permissions the user has:
-	# a: admin, etc.
-	permissions = db.Column(db.String(64))
-	
-	# This is a list of metadata tags useful for filtering; for example, class, year, major, etc.
-	metadata_str = db.Column(db.LargeBinary(4096))
+    """Represents a user."""
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64))
+    email = db.Column(db.String(128), unique=True)
+    password = db.Column(db.LargeBinary(4096))
 
-	def __init__(self, name, password, email):
-		self.name = name
-		self.password = encrypt(SECRET_KEY, password)
-		self.email = email
-		self.permissions = ""
-		self.metadata_str = ""
+    # This is a list of characters designating what permissions the user has:
+    # a: admin, etc.
+    permissions = db.Column(db.String(64))
+
+    # This is a list of metadata tags useful for filtering; for example, class, year, major, etc.
+    metadata_str = db.Column(db.LargeBinary(4096))
+
+    def __init__(self, name, password, email):
+        self.name = name
+        self.password = encrypt(SECRET_KEY, password)
+        self.email = email
+        self.permissions = ""
+        self.metadata_str = ""
 
 
 # Many to Many Relationship between events and users
@@ -75,19 +75,14 @@ class Task(db.Model):
 ##################################################
 # Helper functions
 ##################################################
-	
+
 # We need one of these
 @login_manager.user_loader
 def get_user(id=None, name=None, email=None):
-	if id:
-		return User.query.filter_by(id=int(id)).first()
-	if name:
-		return User.query.filter_by(name=name).first()
-	if email:
-		return User.query.filter_by(email=email).first()
-	return None
-	
-
-	
-	
-
+    if id:
+        return User.query.filter_by(id=int(id)).first()
+    if name:
+        return User.query.filter_by(name=name).first()
+    if email:
+        return User.query.filter_by(email=email).first()
+    return None
--- app/views.py
+++ app/views.py
@@ -15,71 +15,71 @@ import os, json
 @app.route('/index')
 @login_required
 def index():
-	return render_template('index.html', user=current_user)
+    return render_template('index.html', user=current_user)
 
 @app.route('/profile')
 @login_required
 def profile():
-	return render_template('profile.html', user=current_user, target=current_user)
+    return render_template('profile.html', user=current_user, target=current_user)
 
 @app.route('/profile/<user_id>')
 @login_required
 def profile_id(user_id):
-	user = get_user(id=user_id)
-	if not user:
-		flash("Invalid page.")
-		return redirect(url_for('index'))		
-	return render_template('profile.html', user=current_user, target=user)
+    user = get_user(id=user_id)
+    if not user:
+        flash("Invalid page.")
+        return redirect(url_for('index'))
+    return render_template('profile.html', user=current_user, target=user)
 
 #####################################
-# public API 
+# public API
 #####################################
 
 def generate_json_response(file, index):
-	SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
-	json_url = os.path.join(SITE_ROOT, "dummy_data", file)
-	data = json.load(open(json_url))[index]
-	resp = Response(response=json.dumps(data),
-		status=200, \
-		mimetype="application/json")
-	return resp
+    SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
+    json_url = os.path.join(SITE_ROOT, "dummy_data", file)
+    data = json.load(open(json_url))[index]
+    resp = Response(response=json.dumps(data),
+        status=200, \
+        mimetype="application/json")
+    return resp
 
 @app.route('/event/<event_id>', methods=["GET", "PUT"])
 @login_required
 def event(event_id):
-	if request.method == "PUT":
-		# update event
-		pass
+    if request.method == "PUT":
+        # update event
+        pass
 
-	elif request.method == "GET":
-		# get info for event
-		resp = generate_json_response("events.json", int(event_id))
-		return (resp)
-		
+    elif request.method == "GET":
+        # get info for event
+        resp = generate_json_response("events.json", int(event_id))
+        return (resp)
+
 
 @app.route('/task/<task_id>', methods=["GET", "PUT"])
 @login_required
 def task(task_id):
-	if request.method == "PUT":
-		# update task
-		pass
+    if request.method == "PUT":
+        # update task
+        pass
 
-	elif request.method == "GET":
-		# get info for task
-		resp = generate_json_response("tasks.json", int(task_id))
-		return (resp)
+    elif request.method == "GET":
+        # get info for task
+        resp = generate_json_response("tasks.json", int(task_id))
+        return (resp)
 
 
 @app.route('/user/<user_id>', methods=["GET", "PUT"])
 @login_required
 def user(user_id):
-	if request.method == "PUT":
-		# update event
-		pass
+    if request.method == "PUT":
+        # update event
+        pass
 
-	elif request.method == "GET":
-		# get info for event
-		pass
+    elif request.method == "GET":
+        # get info for event
+        pass
 
 
 #####################################
@@ -88,74 +88,74 @@ def user(user_id):
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
-	if request.method == "POST":
-		user = get_user(email=request.form["email"])
-		if user:
-			password = request.form["password"]
-			if password == decrypt(SECRET_KEY, user.password).decode('utf8'):
-				remember = request.form.get("remember", "no") == "yes"
-				if login_user(user, remember=remember):
-					flash("Logged in!")
-					return redirect(request.args.get("next") or url_for("index"))
-				else:
-					flash("Sorry, but you could not log in.")
-			else:
-				flash("Sorry, wrong password.")
-		else:
-			flash("Invalid username.")
-	return render_template('login.html')
+    if request.method == "POST":
+        user = get_user(email=request.form["email"])
+        if user:
+            password = request.form["password"]
+            if password == decrypt(SECRET_KEY, user.password).decode('utf8'):
+                remember = request.form.get("remember", "no") == "yes"
+                if login_user(user, remember=remember):
+                    flash("Logged in!")
+                    return redirect(request.args.get("next") or url_for("index"))
+                else:
+                    flash("Sorry, but you could not log in.")
+            else:
+                flash("Sorry, wrong password.")
+        else:
+            flash("Invalid username.")
+    return render_template('login.html')
 
 @app.route("/register-user", methods=["GET", "POST"])
 def create():
-	if request.method == "POST":
-		if check_valid_new_account(request.form["email"]):
-			new_user = User(request.form["name"], request.form["password"], request.form["email"])
-			db.session.add(new_user)
-			db.session.commit()
-			flash("User "+new_user.name+" registered.")
-			return redirect(url_for("index"))
-	return render_template('create.html')
+    if request.method == "POST":
+        if check_valid_new_account(request.form["email"]):
+            new_user = User(request.form["name"], request.form["password"], request.form["email"])
+            db.session.add(new_user)
+            db.session.commit()
+            flash("User "+new_user.name+" registered.")
+            return redirect(url_for("index"))
+    return render_template('create.html')
 
 # Checks whether a new account is allowed to be created, and flashes a failure message if not
 def check_valid_new_account(email):
-	for user in User.query.all():
-		if user.email == email:
-			flash("That email address has already been used.")
-			return False
-	return True
-	
+    for user in User.query.all():
+        if user.email == email:
+            flash("That email address has already been used.")
+            return False
+    return True
+
 @app.route("/logout")
 @login_required
 def logout():
     logout_user()
     flash("Logged out.")
     return redirect(url_for("login"))
-    
+
 @app.route('/admin', methods=["GET", "POST"])
 @login_required
 def admin():
-	return require_permission("a", # only admins can do this
-							  lambda :admin_internal())
+    return require_permission("a", # only admins can do this
+                              lambda :admin_internal())
 
 def admin_internal():
-	if request.method == "POST":
-		new_admins = [get_user(id=int(n)) for n in request.form.getlist('new-admins')]
-		new_losers = [get_user(id=int(n)) for n in request.form.getlist('new-losers')]
-		for u in new_admins:
-			u.permissions = request.form["permissions"]
-		for u in new_losers:
-			u.permissions = ""
+    if request.method == "POST":
+        new_admins = [get_user(id=int(n)) for n in request.form.getlist('new-admins')]
+        new_losers = [get_user(id=int(n)) for n in request.form.getlist('new-losers')]
+        for u in new_admins:
+            u.permissions = request.form["permissions"]
+        for u in new_losers:
+            u.permissions = ""
 
-		db.session.commit()
-		flash("Permissions updated.")
-		return redirect(url_for("index"))
+        db.session.commit()
+        flash("Permissions updated.")
+        return redirect(url_for("index"))
 
-	return render_template('admin.html', user=current_user,
-										 user_list=User.query.all())
-    
+    return render_template('admin.html', user=current_user,
+                                         user_list=User.query.all())
+
 # Requires some amount of privileges to view page
 def require_permission(permission_options, viewfun):
-	for perm in permission_options:
-		if perm in current_user.permissions:
-			return viewfun()
-	return render_template('permission-denied.html', user=current_user)
+    for perm in permission_options:
+        if perm in current_user.permissions:
+            return viewfun()
+    return render_template('permission-denied.html', user=current_user)
